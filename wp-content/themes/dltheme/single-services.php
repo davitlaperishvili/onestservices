8fb4969f91e5eff87b05586b701c7e82d0a5777e
dlaperishvili <davitlaperishvili@gmail.com>
2023-09-05 13:09:35 +0400

    service single page is added

---
+++ wp-content/themes/dltheme/single-services.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html >
+<html lang="en" class="dl_theme" >
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./scripts/css/main.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  <title>Document</title>
+</head>
+<body >
+
+  <section class="hero_inside">
+    <figure>
+      <img src="./images/hero_inside.jpg" alt="">
+    </figure>
+    <div class="container">
+      <div class="hero_content">
+        <h1 class="page_title">Comprehensive Day-to-Day Service</h1>
+        <div class="theme_button">
+          <a href="">Maintenance request</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="text_block trans-all-4">
+    <div class="container">
+      <div class="block_wrap">
+        <div class="block_content">
+          <h2 class="section_title"><span>More space <br> for Paragraph</span></h2>
+          <div class="section_text ">
+            <p>Welcome to our revolutionary property management services! We pride ourselves on being unlike any typical property management company you've encountered. Say goodbye to high monthly fees draining your profits and embrace a smarter approach. With us, you stay in control while we provide the services you need, precisely when you need them. </p>
+            <p>Our flexible "pay as you go" model empowers you to save money and maximize your rental return on investment. Experience property management that puts you first – efficient, cost-effective, and tailored to your unique requirements. Manage with ease, and let us handle the rest!</p>
+          </div>
+          <div class="theme_button">
+            <a href="#">Compare Our Prices</a>
+          </div>
+        </div>
+        <div class="block_media">
+          <div class="image_wrap">
+            <figure>
+              <img src="./images/about_us.jpg" alt="">
+            </figure>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="list_points no_bg">
+    <div class="container">
+      <h2 class="section_title center"><span>Experienced and Trusted</span></h2>
+      <div class="section_subtitle center">Our team handles all the day-to-day activities involved in property services, serving as the primary point of contact between tenants, landlords, and contractors. This service includes:</div>
+      <div class="points_list">
+        <div class="point_item">
+          <figure>
+            <img src="./images/service1.svg" alt="">
+          </figure>
+          <div class="point_content">
+            <div class="service_title">Comprehensive Day-to-Day Management</div>
+            <div class="service_desc">Our team handles all the day-to-day activities involved in property management, serving as the primary point of contact between tenants, landlords, and contractors.</div>
+          </div>
+        </div>
+        <div class="point_item">
+          <figure>
+            <img src="./images/service2.svg" alt="">
+          </figure>
+          <div class="point_content">
+            <div class="service_title">Tenant Replacement</div>
+            <div class="service_desc">Our Tenant Replacement service covers all aspects of transitioning between tenants, ensuring a smooth and efficient process.</div>
+          </div>
+        </div>
+        <div class="point_item">
+          <figure>
+            <img src="./images/service3.svg" alt="">
+          </figure>
+          <div class="point_content">
+            <div class="service_title">Tenant Contract Renewal</div>
+            <div class="service_desc">Our Tenant Contract Renewal service is designed to maintain a positive and long-lasting relationship with tenants. We believe that a satisfied tenant is a long-term tenant, and we strive to provide a high level of service to ensure their satisfaction</div>
+          </div>
+        </div>
+        <div class="point_item">
+          <figure>
+            <img src="./images/service4.svg" alt="">
+          </figure>
+          <div class="point_content">
+            <div class="service_title">Trouble Tenant Management</div>
+            <div class="service_desc">Our Trouble Tenant Management service is designed to handle any issues that may arise with tenants, including late rent payments, city or HOA violations, refusal to vacate at the end of the lease term, and, if necessary, eviction proceedings.</div>
+          </div>
+        </div>
+      </div>
+      <div class="section_note">This service ensures a smooth, efficient process for all parties involved, saving landlords time and stress.</div>
+    </div>
+  </section>
+
+  <section class="our_services no_bg">
+    <div class="container">
+      <h2 class="section_title center">Our <span>Services</span></h2>
+      <div class="services_list service_links">
+        <div class="service_item">
+          <a href="#"></a>
+          <figure>
+            <img src="./images/service1.svg" alt="">
+          </figure>
+          <div class="service_title">Comprehensive Day-to-Day Management</div>
+          <div class="service_desc">Our team handles all the day-to-day activities involved in property management, serving as the primary point of contact between tenants, landlords, and contractors.</div>
+        </div>
+        <div class="service_item">
+          <a href="#"></a>
+          <figure>
+            <img src="./images/service2.svg" alt="">
+          </figure>
+          <div class="service_title">Tenant Replacement</div>
+          <div class="service_desc">Our Tenant Replacement service covers all aspects of transitioning between tenants, ensuring a smooth and efficient process.</div>
+        </div>
+        <div class="service_item">
+          <a href="#"></a>
+          <figure>
+            <img src="./images/service3.svg" alt="">
+          </figure>
+          <div class="service_title">Tenant Contract Renewal</div>
+          <div class="service_desc">Our Tenant Contract Renewal service is designed to maintain a positive and long-lasting relationship with tenants. We believe that a satisfied tenant is a long-term tenant, and we strive to provide a high level of service to ensure their satisfaction</div>
+        </div>
+        <div class="service_item">
+          <a href="#"></a>
+          <figure>
+            <img src="./images/service4.svg" alt="">
+          </figure>
+          <div class="service_title">Trouble Tenant Management</div>
+          <div class="service_desc">Our Trouble Tenant Management service is designed to handle any issues that may arise with tenants, including late rent payments, city or HOA violations, refusal to vacate at the end of the lease term, and, if necessary, eviction proceedings.</div>
+        </div>
+        <div class="service_item">
+          <a href="#"></a>
+          <figure>
+            <img src="./images/service3.svg" alt="">
+          </figure>
+          <div class="service_title">Tenant Contract Renewal</div>
+          <div class="service_desc">Our Tenant Contract Renewal service is designed to maintain a positive and long-lasting relationship with tenants. We believe that a satisfied tenant is a long-term tenant, and we strive to provide a high level of service to ensure their satisfaction</div>
+        </div>
+        <div class="service_item">
+          <a href="#"></a>
+          <figure>
+            <img src="./images/service2.svg" alt="">
+          </figure>
+          <div class="service_title">Tenant Replacement</div>
+          <div class="service_desc">Our Tenant Replacement service covers all aspects of transitioning between tenants, ensuring a smooth and efficient process.</div>
+        </div>
+        <div class="service_item">
+          <a href="#"></a>
+          <figure>
+            <img src="./images/service3.svg" alt="">
+          </figure>
+          <div class="service_title">Tenant Contract Renewal</div>
+          <div class="service_desc">Our Tenant Contract Renewal service is designed to maintain a positive and long-lasting relationship with tenants. We believe that a satisfied tenant is a long-term tenant, and we strive to provide a high level of service to ensure their satisfaction</div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <footer class="trans-all-4">
+    <div class="footer_wrap">
+      <div class="container">
+        <div class="footer_columns">
+          <div class="column">
+            <div class="column_title">Contact us</div>
+            <div class="footer_contact_info">
+              <div class="info_item">
+                <div class="svg_wrap">
+                  <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g opacity="0.9">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M7.26135 17.3176L10.3744 14.2041C10.8085 13.7699 11.3522 13.5664 11.9646 13.6085C12.577 13.6507 13.0877 13.9271 13.4581 14.4166L18.0679 20.5097C18.6893 21.331 18.611 22.4676 17.8827 23.1959L15.7252 25.3537C19.0452 28.1764 21.8211 30.9536 24.6433 34.2735L26.8013 32.1152C27.5296 31.3868 28.666 31.3086 29.4872 31.9301L35.5794 36.5405C36.0689 36.9109 36.3452 37.4216 36.3874 38.0341C36.4296 38.6466 36.226 39.1904 35.7919 39.6245L32.6783 42.7385C25.5089 49.909 0.0917082 24.4882 7.26135 17.3176ZM33.8537 6C39.4572 6 43.9997 10.2467 43.9997 15.4854C43.9997 20.724 39.4572 24.9707 33.8537 24.9707C31.9295 24.9707 30.1305 24.4699 28.5975 23.6002C27.3492 24.1528 26.0098 24.3125 24.728 24.123C25.208 23.2144 25.5656 22.2462 25.7915 21.2437C24.4847 19.647 23.7077 17.6511 23.7077 15.4854C23.7077 10.2467 28.2502 6 33.8537 6Z" stroke="white" stroke-miterlimit="22.9256" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M33.8308 20.0547V14.4339H32.5506M32.4907 20.1118H35.171M33.8308 10.73V10.759" stroke="white" stroke-miterlimit="22.9256" stroke-linecap="round" stroke-linejoin="round"/>
+                    </g>
+                  </svg>
+                </div>
+                <div class="info_content">
+                  <div class="info_title">Hotline:</div>
+                  <div class="info_value">
+                    <a href="tel:(201) 555-0124">(201) 555-0124</a>
+                  </div>
+                </div>
+              </div>
+              <div class="info_item">
+                <div class="svg_wrap">
+                  <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g opacity="0.9">
+                    <mask id="mask0_260_2400" style="mask-type:luminance" maskUnits="userSpaceOnUse" x="6" y="6" width="39" height="38">
+                    <path d="M43.5001 43.5V6.49987H6.5V43.5H43.5001Z" fill="white" stroke="white"/>
+                    </mask>
+                    <g mask="url(#mask0_260_2400)">
+                    <path d="M27.1877 21.4998C27.1877 23.358 26.0474 24.7495 24.682 24.7495C23.3168 24.7495 22.1094 23.3773 22.1094 21.5191C22.1094 19.6609 23.3839 18.4228 24.7492 18.4228C26.1145 18.4228 27.1877 19.6416 27.1877 21.4998Z" stroke="white" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M24.8679 27.8945C22.1523 27.8194 19.6762 25.9815 18.941 23.1859C18.0924 19.9591 19.9484 16.5889 23.1277 15.5769C26.4902 14.5065 30.0663 16.3982 31.0823 19.7644C31.4418 21.1114 31.232 22.2832 30.7556 23.481C30.5667 23.956 29.9438 25.0679 28.558 25.0679C27.8015 25.0679 27.1707 24.3429 27.1749 23.5078L27.2023 17.9739" stroke="white" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M31.0828 19.7644C31.4423 21.1113 31.2325 22.2831 30.7561 23.481" stroke="white" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M30.3659 11.5664L24.9998 7.11327L19.6338 11.5664" stroke="white" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M12.3828 17.5839L7.11328 21.957V42.8867H42.8868V21.957L37.6173 17.5839" stroke="white" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M25 33.0898L37.6173 25.2368V11.5663H12.3828V25.2368L25 33.0898Z" stroke="white" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M42.8868 21.957L25.0001 33.0898L7.11328 21.957V42.8867H42.8868V21.957Z" stroke="white" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                    </g>
+                    </g>
+                  </svg>
+                </div>
+                <div class="info_content">
+                  <div class="info_title">Email:</div>
+                  <div class="info_value">
+                    <a href="mailto:Realestatecp@gmail.com">Realestatecp@gmail.com</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="column middle_column">
+            <div class="column_title">Office Address</div>
+            <div class="footer_contact_info">
+              <div class="info_item">
+                <div class="info_content">
+                  <div class="info_title">Head office:</div>
+                  <div class="info_value">Syracuse, Connecticut 35624</div>
+                </div>
+              </div>
+              <div class="info_item">
+                <div class="info_content">
+                  <div class="info_title">Branch:</div>
+                  <div class="info_value">St. Richardson, California 62639 <br> <hr> St. Utica, Pennsylvania 57867</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          <div class="column">
+            <div class="column_title">Our Company</div>
+            <div class="footer_url_list">
+              <ul>
+                <li>
+                  <a href="">Management services</a>
+                </li>
+                <li>
+                  <a href="">About Us</a>
+                </li>
+                <li>
+                  <a href="">Owners</a>
+                </li>
+                <li>
+                  <a href="">Terms Of Use</a>
+                </li>
+                <li>
+                  <a href="">Privacy Policy</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="column">
+            <div class="footer_socuals_list">
+              <div class="socials">
+                <ul>
+                  <li>
+                    <a href="#">
+                      <i class="ri-facebook-fill"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="ri-twitter-fill"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="ri-linkedin-fill"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="ri-instagram-fill"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="footer_menu">
+          <ul>
+            <li>
+              <a href="#">About us</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">Prices</a>
+            </li>
+            <li>
+              <a href="#">Our Guarantee</a>
+            </li>
+          </ul>
+        </div>
+        <div class="copyright">Copyright © 2023 Onest LTD</div>
+      </div>
+    </div>
+  </footer>
+  <script src="./scripts/js/main.js"></script>
+</body>
+</html>
